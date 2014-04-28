449ac9d5330808756fcffe8adfbfde894978aa89
lukelakea <lukelakea@gmail.com>
2014-04-28 19:07:04 -0400

    First Draft without GUI

---
+++ Deck.java
@@ -0,0 +1,76 @@
+import java.util.Random;
+public class Deck 
+{
+   final int CARDS_IN_DECK = 52;
+	final int EMPTY_DECK = 0;
+
+   Card [] deck;
+   int ct;
+	
+	
+   public Deck()
+   {
+      freshDeck();
+   }
+	
+	public Deck(boolean isEmpty)
+	{
+		deck = new Card[EMPTY_DECK];
+	}
+	
+   public void freshDeck()
+   {
+      deck = new Card[CARDS_IN_DECK];
+      for (int r = Card.ACE; r<=Card.KING;r++)
+      {
+         for (int s=Card.SPADES;s<=Card.CLUBS;s++)
+         {
+            deck[ct]=new Card(r,s);
+            ct = ct + 1;
+         }
+      }
+     
+   
+   }
+   public Card dealCard()
+   {
+      ct--;
+      return deck[ct];
+   }
+	
+	
+	
+   public int cardsRemaining()
+   {  
+      return ct;
+   }
+   public void shuffle()
+   {
+      int randNum;
+      Card temp;
+      Random r = new Random();
+      for (int i = 0; i < ct; i++)
+      {
+         randNum = r.nextInt(ct);
+         temp = deck[i];
+         deck[i]=deck[randNum];
+         deck[randNum]=temp;
+      }
+   }
+   public boolean isEmpty()
+   {
+      return (cardsRemaining() == 0);
+   }
+   public static void main(String [] args) 
+   {
+      Deck deck = new Deck();
+      deck.shuffle();
+      int i = 0;
+//      while (deck.cardsRemaining() > 0)
+      while (!(deck.isEmpty()))
+         System.out.println((i++)+1 + " : " + deck.dealCard().toString());
+      deck.freshDeck();
+
+   }
+}
+
